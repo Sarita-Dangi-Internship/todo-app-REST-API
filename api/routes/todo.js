0df72870cc98dd04b6e6a5e7ea9e342b02052442
Sarita <dsarritta@gmail.com>
2021-06-23 17:18:50 +0545

    create route: create routes for patch and delete method

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -21,7 +21,13 @@ router.post('/', async (req, res) => {
 })
 
 router.patch('/:todo_id', async (req, res) => {
-    
+    const todo = await Todo.findByIdAndUpdate(req.params.todo_id,{ task: req.body.task })
+    return res.status(200).send(todo);
+})
+
+router.delete('/:todo_id', async (req, res) => {
+    const todo = await Todo.findByIdAndDelete(req.params.todo_id)
+    return res.status(200).send(todo);
 })
 
 module.exports = router
