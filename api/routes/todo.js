10737ba430d5968316d6545992151327c6b42d1e
Sarita <dsarritta@gmail.com>
2021-06-23 20:12:52 +0545

    error handle: add try catch block

--- api/routes/todo.js
+++ api/routes/todo.js
@@ -1,33 +1,49 @@
-const express = require('express');
+const express = require("express");
 
-const {Todo} = require('../model/todo');
+const { Todo } = require("../model/todo");
 
 const router = express.Router();
 
-router.get('/', async(req, res) => {
-    const todo = await Todo.find()
-    if(!todo) return res.status(401).send("No todo found")
-    return res.status(200).send(todo)
-})
+router.get("/", async (req, res) => {
+  try {
+    const todo = await Todo.find();
+    if (!todo) return res.status(401).send("No todo found");
+    return res.status(200).send(todo);
+  } catch (error) {}
+});
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  try {
     const todo = new Todo({
-        task: req.body.task,
-        createdDate: req.body.createdDate,
-        completed: req.body.completed,
-    })
+      task: req.body.task,
+      createdDate: req.body.createdDate,
+      completed: req.body.completed,
+    });
     const result = await todo.save();
     return res.status(200).send(result);
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.patch('/:todo_id', async (req, res) => {
-    const todo = await Todo.findByIdAndUpdate(req.params.todo_id,{ task: req.body.task })
+router.patch("/:todo_id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.todo_id, {
+      task: req.body.task,
+    });
     return res.status(200).send(todo);
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.delete('/:todo_id', async (req, res) => {
-    const todo = await Todo.findByIdAndDelete(req.params.todo_id)
+router.delete("/:todo_id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.todo_id);
     return res.status(200).send(todo);
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
